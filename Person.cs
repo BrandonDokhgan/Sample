a8b5323a326d1e59f3a636c292d26aee7f752768
BrandonDokhgan <50833709+BrandonDokhgan@users.noreply.github.com>
2019-05-20 10:58:53 -0400

    Add files via upload

--- Person.cs
+++ Person.cs
@@ -1,17 +1,24 @@
-public class Person{
+public class Person
+{
 
-    public string FirstName {get;set;}
-    public string LastName {get;set;}
-    public string StreetAddress {get;set;}
-    public string City {get;set;}
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string StreetAddress { get; set; }
+    public string City { get; set; }
 
     public Person()
     { }
 
     public Person(string firstName, string lastName)
-    { 
+    {
         FirstName = firstName;
         LastName = lastName;
+    }
+
+    public void UpdateAddress(string address)
+    {
+        if (!StreetAddress.Equals(address) && !string.IsNullOrEmpty(address))
+            StreetAddress = address;
     }
 
     
